80eeab1bcfc80f8718cf1974f7fac8b4834af9fb
Guto Xavier <gutoffline@gmail.com>
2019-01-22 22:21:43 -0200

    Calabouços e Dragões

--- 2019/Janeiro/CalaboucosEDragoes/CalaboucosEDragoes/Program.cs
+++ 2019/Janeiro/CalaboucosEDragoes/CalaboucosEDragoes/Program.cs
@@ -11,9 +11,11 @@ namespace CalaboucosEDragoes
         static void Main(string[] args)
         {
             string nome="", classe="", raca = "";
-            int vida = 100;
+            string resposta = "";
+            int vida = 100 , dado_monstro, dado_jogador;
+            Random dado = new Random();
 
-            Console.WriteLine("Informe seu nome aventureiro.");
+            Console.WriteLine("Informe seu nome aventureiro(a).");
             nome = Console.ReadLine();
 
             Console.WriteLine("Informe sua raça.");
@@ -29,15 +31,68 @@ namespace CalaboucosEDragoes
             Console.WriteLine(raca);
             Console.WriteLine("---------------------------------");
 
-            string resposta = "";
-            Console.WriteLine("Deseja entrar na porta a sua direita?");
+            Console.WriteLine("Deseja entrar na porta a sua esquerda? (S/N)");
             resposta = Console.ReadLine();
 
-            if(resposta == "sim")
-            {
-                
+            if(resposta == "S"){
+                Console.WriteLine("Aqui tem um monstro. Você vai enfrentá-lo!!!");  
+
+                dado_monstro = dado.Next(1, 6);
+                dado_jogador = dado.Next(1, 6);
+
+                Console.WriteLine("Monstro: " + dado_monstro + " - Jogador: " + dado_jogador);
+
+                if(dado_monstro > dado_jogador){
+                    Console.WriteLine("Monstro Venceu!!!");
+                    vida = vida - 50;
+                }else{
+                    Console.WriteLine("Jogador Venceu!!!");
+                }
 
             }
+            Console.WriteLine("Sua vida é: " + vida);
+
+            Console.WriteLine("Deseja entrar na salas à sua esquerda? (S/N)");
+            resposta = Console.ReadLine();
+
+            if(resposta == "S")
+            {
+                Console.WriteLine("Você ganhou vida extra!!!");
+                vida = vida + 30;
+            }
+
+            Console.WriteLine("Sua vida é: " + vida);
+
+            Console.WriteLine("Mais uma sala, você deseja entrar na sala? (S/N) ");
+            resposta = Console.ReadLine();
+
+            if (resposta == "S")
+            {
+                Console.WriteLine("Eita, esse monstro é grande, logo ele é monstrão!!!(Birl)");
+                dado_monstro = dado.Next(1, 8);
+                dado_jogador = dado.Next(1, 6);
+
+                if (dado_monstro > dado_jogador)
+                {
+                    Console.WriteLine("Ele te acertou!!!");
+                    vida = vida - 60;
+                }
+                else
+                {
+                    Console.WriteLine("Você é o bixão mesmo, matou o monstrão");
+                }
+            }
+            Console.WriteLine("Vida " + vida);
+            if(vida <= 0)
+            {
+                Console.WriteLine("Vacilou, você morreu...guenta nada...");
+            }
+            else
+            {
+                Console.WriteLine("Continua vivo, merece um abraço...mas não!");
+            }
+            
+            Console.ReadKey();
         }
     }
 }
